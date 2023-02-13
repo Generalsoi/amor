4c6e7c9f08a3eef6e6b3cf2d3f5872145d3a0f9a
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-14 00:35:20 +0100

    fix: minor updates

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export const Homepage: FC = () => {
   return (
     <div className="font-ibmPlexMono w-[100%] h-screen md:flex relative">
-      <div className="md:w-[50%] h-[50%] md:h-full flex flex-col items-center justify-center animate-pulse">
+      <div className="md:w-[50%] h-[50%] md:h-full flex flex-col items-center justify-center animate-pulse border-2 border-black m-2 rounded-lg">
         <h1 className="md:text-7xl text-4xl font-extrabold">How are you?</h1>
         <h1 className="md:text-7xl text-4xl font-extrabold">My love</h1>
       </div>
@@ -16,8 +16,8 @@ export const Homepage: FC = () => {
         alt="my-baby"
       />
 
-      <div className="absolute md:bottom-4 md:right-[50%] md:top-auto top-8 px-4 right-4 flex items-center gap-3">
-        <p>Click here to view some goodies</p>
+      <div className="absolute md:bottom-4 md:right-[50%] md:top-auto top-8 px-4 right-3 flex items-center gap-2">
+        <p>Click here to view something cool</p>
         <Link to="/details">
           <button className="cursor-pointer animate-bounce px-2 py-2 border-2 border-black rounded-full">
             <AiOutlineArrowRight className="text-3xl" />
